2066267e0cfa1690c4dc0790abaea6258feafad3
JustRizky02 <rizky30054@gmail.com>
2023-11-22 00:19:24 +0700

    tambah details

--- lib/bottombar/bottom_navbar.dart
+++ lib/bottombar/bottom_navbar.dart
@@ -4,6 +4,7 @@ import 'package:rolling_bottom_bar/rolling_bottom_bar_item.dart';
 import 'package:wheather/pages/details.dart';
 import 'package:wheather/pages/grafik.dart';
 import 'package:wheather/pages/home.dart';
+import 'package:wheather/pages/info.dart';
 
 class BottomNavbar extends StatefulWidget {
   const BottomNavbar({Key? key}) : super(key: key);
@@ -25,6 +26,7 @@ class _BottomNavbarState extends State<BottomNavbar> {
             Home(),
             Details(),
             Grafik(),
+            Info(),
           ],
         ),
         extendBody: true,
@@ -40,6 +42,8 @@ class _BottomNavbarState extends State<BottomNavbar> {
                 label: 'Details', activeColor: Color(0xff001449)),
             RollingBottomBarItem(Icons.area_chart_outlined,
                 label: 'Grafik', activeColor: Color(0xff001449)),
+            RollingBottomBarItem(Icons.question_mark,
+                label: 'Info', activeColor: Color(0xff001449)),
           ],
           enableIconRotation: true,
           onTap: (index) {
--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Details extends StatefulWidget {
   const Details({Key? key}) : super(key: key);
@@ -10,6 +11,119 @@ class Details extends StatefulWidget {
 class _DetailsState extends State<Details> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Color(0xff005bc5),
+          ),
+          child: Center(
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment:
+                    CrossAxisAlignment.center, // Center align horizontally
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        text: 'Montreal',
+                        style: GoogleFonts.inter(
+                          fontSize: 39,
+                          color: Colors.white,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: '\n26℃ || Mostly Clear',
+                            style: GoogleFonts.inter(
+                              fontSize: 22,
+                              color: Colors.grey.shade400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          color: Color(0x66000000).withOpacity(0.2),
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              topRight: Radius.circular(30),
+                              bottomLeft: Radius.circular(0),
+                              bottomRight: Radius.circular(0))),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 0.0, horizontal: 20.0),
+                            child: Container(
+                              width: double.infinity,
+                              height: 150.0,
+                              decoration: BoxDecoration(
+                                  color: Color(0x66008080).withOpacity(0.2),
+                                  borderRadius: BorderRadius.circular(30)),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                width: 165.0,
+                                height: 165.0,
+                                decoration: BoxDecoration(
+                                  color: Color(0x66008080).withOpacity(0.2),
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                              ),
+                              SizedBox(width: 20),
+                              Container(
+                                width: 165.0,
+                                height: 165.0,
+                                decoration: BoxDecoration(
+                                  color: Color(0x66008080).withOpacity(0.2),
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                width: 165.0,
+                                height: 165.0,
+                                decoration: BoxDecoration(
+                                  color: Color(0x66008080).withOpacity(0.2),
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                              ),
+                              SizedBox(width: 20),
+                              Container(
+                                width: 165.0,
+                                height: 165.0,
+                                decoration: BoxDecoration(
+                                  color: Color(0x66008080).withOpacity(0.2),
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
